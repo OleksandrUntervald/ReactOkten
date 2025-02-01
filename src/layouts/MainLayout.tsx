9c3f5221840e678bf92fb285913cd81a5a8a8261
OleksandrUntervald <untervaldthebest@gmail.com>
2025-02-01 23:07:42 +0200

    Lesson-3 HW-Task3 - done

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,9 @@
 import {Outlet} from "react-router"
+import {MenuComponent} from "../components/menu/MenuComponent.tsx";
 export const MainLayout = () => {
     return (
-        <div><Outlet/></div>
+        <div>
+            <MenuComponent/>
+            <Outlet/></div>
     );
 };
